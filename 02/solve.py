45a37f535d5d9198450b81ef7779d55b97732136
Tim Nosco <tim.nosco@gmail.com>
2018-04-12 14:21:06 -0400

    comment clarity

--- 02/solve.py
+++ 02/solve.py
@@ -24,7 +24,7 @@ f = p.factory.callable(0x080484bb)
 hash_len = 0x20
 #the first four characters will be something like a-0\0 or Z-20
 var_len = 4
-#fin will hold our symbolic hash array
+#fin will hold our symbolic hash input array
 fin = claripy.BVS('sym_arg', 8*hash_len)
 #call the function with the array and 0x20
 res = f(fin, hash_len)
@@ -35,18 +35,20 @@ var_chr = fin.get_bytes(0,1)
 #it should be a letter, digit, or punctuation character
 state.add_constraints(var_chr >= min(all_chars),
  					  var_chr <= max(all_chars))
+#the remainder of the input array
+remainder = fin.get_bytes(1,hash_len-1)
 
 #flag will collect our solved constraints
 flag = ""
 #there is a hardcoded lenth of 44 characters
 for i in range(44):
-	#format the fixed part of the input to be like c-HH where c is the char
+	#format the first part of the input is c-HH where c is the char
 	# to learn and HH is the hex value of the current index, i
 	fixed = "-{}".format(hex(i)[2:]).ljust(hash_len-1,'\0')
 	#the hash results are stored in an unsigned int array starting at 0x08049a00
 	ith_hash = state.mem[0x08049a00+4*i].long.concrete
-	tmp=[res == ith_hash,
-		fin.get_bytes(1,hash_len-1) == fixed]
+	#these constraints are temp because we only need them for this i
+	tmp = [res == ith_hash, remainder == fixed]
 	#ask z3 to solve for the value of the flag byte
 	e = state.solver.eval(var_chr, extra_constraints=tmp)
 	#record the solver's response
