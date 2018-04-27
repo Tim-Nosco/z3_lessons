29e6128b3c7cfe6f9806f72dd560c5a5c6823e8c
Tim Nosco <tim.nosco@gmail.com>
2018-04-27 10:37:32 -0400

    gets to wall 10 lol

--- 08/extract_z3_solver.py
+++ 08/extract_z3_solver.py
@@ -1,7 +1,6 @@
 import angr, claripy, z3
 import logging
 import subprocess
-from functools import partial
 
 angr_logger = logging.getLogger()
 angr_logger.setLevel(logging.INFO)
@@ -73,60 +72,51 @@ def wall2():
 	s = p.factory.blank_state(remove_options={angr.options.COMPOSITE_SOLVER})
 	#ensure the rk start values are 0
 	s.add_constraints(*[k!='\0' for k in keyarr])
+	rk_start = s.regs.rbp + 32
+	for i in range(60):
+		s.memory.store(rk_start+4*i, claripy.BVV(0,8*4))
 
 	logger.info("starting symbolic execution on aes")	
-	aes_addr = p.loader.find_symbol('malicious_aes_test').rebased_addr
+	aes_addr = p.loader.find_symbol('rijndaelKeySetupEnc').rebased_addr
 	aes = p.factory.callable(aes_addr, base_state=s)
-	r = aes(keyarr)
+	r = aes(rk_start,keyarr,0x80) #todo args are int*, char*, int?
 	s = aes.result_state
-	s.add_constraints(r==3)
+	rk_final = [s.memory.load(rk_start+4*i,4) for i in range(60)]
+
+	magic = 0x28
+	s.add_constraints(	rk_final[magic+0]==0x048a97a0,
+						rk_final[magic+1]==0xac9a53b7,
+						rk_final[magic+2]==0xd37fd65b,
+						rk_final[magic+3]==0x15cf1362)
 
 	#make the Te4 lookup table
 	Te4 = p.loader.find_symbol("Te4").rebased_addr
-	Te4_table = [s.mem[Te4+x*4].uint8_t.concrete for x in range(256)]
-	logger.info("Starting symbolic lookups.")
-	total = str(len(table_lookups)).zfill(2)
-	def builder(offset,a,x):
-		return claripy.If(x==offset, Te4_table[x], a)
-	for i,e in enumerate(table_lookups):
-		k,v = e
-		logger.info("Doing lookup number %s/%s", str(i+1).zfill(2), total)
-		mux = reduce(partial(builder,k),range(256),claripy.BVV(0,8))
-		s.add_constraints(mux==v)
+	Te4_table = [(x,s.mem[Te4+x*4].uint8_t.concrete) for x in range(256)]
+	z3_table = z3.Function("Te4", z3.BitVecSort(8), z3.BitVecSort(8))
+	z3_solver = s.solver._solver._get_solver()
+	for i, e in Te4_table:
+		z3_solver.add(z3_table(i)==e)
+	global table_lookups
+	for idx, res in table_lookups:
+		idx = claripy.backends.z3.convert(idx)
+		res = claripy.backends.z3.convert(res)
+		z3_solver.add(z3_table(idx)==res)
+	logger.info("Checking satisfiability")
+	logger.info(z3_solver.check())
+	logger.info("Getting model")
+	m = z3_solver.model()
+	z3key = claripy.backends.z3.convert(key)
+	resolved_key = hex(m[z3key].as_long())[2:].replace('L','').decode('hex')
+	logger.info("KEY: {}".format(repr(resolved_key)))
+	#KEY: '7\xf0\x18T\xb7\x04S\xf9\x99\x0cy\xeb\x96\xed^\xb6'
 
-	logger.info("Done with lookups.")
-
-	logger.info("Evaluating key.")
-	resolved_key = s.solver.eval(key,cast_to=str)
-	logger.info("FOUND: %s", repr(resolved_key))
-	# FOUND: 'ACHIEVEMENTAWARD'
-	return [resolved_key]
-
-def wall10():
-	global p
-	logger.info("setting up sym args")
-	keylen = 0x1d
-	key = claripy.BVS('key', 8*keylen)
-	keyarr = [key.get_byte(i) for i in range(keylen)]
-	s = p.factory.blank_state()
-	s.add_constraints(*[k!='\0' for k in keyarr])
-
-	logger.info("starting symbolic execution on crc")	
-	crc_addr = p.loader.find_symbol('crc32_test').rebased_addr
-	crc = p.factory.callable(crc_addr, base_state=s)
-	r = crc(keyarr)
-	s = crc.result_state
-	s.add_constraints(r==3)
-	logger.info("Checking satisfiabliity")
-	resolved_key = s.solver.eval(key,cast_to=str)
-	logger.info("FOUND: %s", repr(resolved_key))
-	return [resolved_key]
+	hook(locals())
 
 argv = [p.filename]
 argv += ('174','116')# wall1()
-argv += ['ACHIEVEMENTAWARD']#wall2()
-argv += ['a']*5
-argv += ['B'*5]
-argv += wall10()
+argv += wall2()
 test_run(argv)
 argv +=[claripy.BVS("argv{}".format(i),8*3) for i in range(len(argv),wall_zero+1)]
+# s = p.factory.full_init_state(args=argv, add_options=angr.options.unicorn)
+# sm = p.factory.simgr(s)
+# print sm.explore(find=0x0400905)
