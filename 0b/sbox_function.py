4cc716a8b42d6489e4393103b1e8929edf1db01e
Tim Nosco <tim.nosco@gmail.com>
2018-10-20 12:45:06 -0400

    attempt at distinct sbox

--- 0b/sbox_function.py
+++ 0b/sbox_function.py
@@ -18,12 +18,18 @@ def hook(l=None):
 	IPython.embed(banner1="",confirm_exit=False)
 	exit()
 
-def main():
+def make_vecs():
 	upper_half = z3.Function("u", z3.BitVecSort(MAXBITS), 
 								z3.BitVecSort(MAXBITS), z3.BitVecSort(MAXBITS))
 	lower_half = z3.Function("l", z3.BitVecSort(MAXBITS), 
 								z3.BitVecSort(MAXBITS), z3.BitVecSort(MAXBITS))
-	s = z3.Solver()
+	mask = ((1<<MAXBITS)-1)
+	merged = [z3.Concat(upper_half((i>>MAXBITS)&mask, i&mask), 
+						lower_half((i>>MAXBITS)&mask, i&mask)) for i in range(2**(MAXBITS*2))]
+	return upper_half,lower_half,merged
+
+def make_sbox(s, vecs=None):
+	upper_half,lower_half,_ = make_vecs() if vecs==None else vecs
 	#rows and columns
 	for i in range(MAXVAL):
 		#upper
@@ -60,6 +66,16 @@ def main():
 		print row
 	with open('/tmp/save.txt', 'w') as f:
 		f.write(''.join(fmt_sbox_hex))
+	return sbox
+
+def main():
+	sbox1 = make_sbox(z3.Solver())
+	# s = z3.Solver()
+	# k = z3.BitVec("k", 6)
+	# u,l,sbox2 = make_vecs()
+	# s.add(z3.ForAll([k], 
+	# 	z3.Or(*(k^sbox1[x]!=sbox2[x] for x in range(2**(MAXBITS*2))))))
+	# sbox2 = make_sbox(s,(u,l,None))
 	hook(locals())
 
 if __name__ == '__main__':
